f0bc70baab082ab2ee849efd5b244376b78fae16
Got-crypto <pecpalmplay@gmail.com>
2023-08-01 21:33:05 +0200

    added sanity client

--- next.config.js
+++ next.config.js
@@ -6,7 +6,9 @@ const nextConfig = {
   },
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_PROJECT_TOKEN: process.env.SANITY_PROJECT_TOKEN,
   }
 }
 
