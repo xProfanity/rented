95e3ea04205619c42ea1f6d00e2324e555199d04
Got-crypto <pecpalmplay@gmail.com>
2023-08-19 23:21:01 +0200

    added whyUs content constants

--- assets/constants/index.js
+++ assets/constants/index.js
@@ -48,3 +48,34 @@ export const ratingStars = [
         isActive: false
     },
 ]
+
+export const whyUs = [
+    {
+        name: 'Quality Listings',
+        description: "Discover exceptional homes that are well-maintained and desirable. Our curated listings ensure you'll find a place you'll love to call home."
+    },
+    {
+        name: 'Expertise',
+        description: "Our experienced team understands the local real estate market inside out. Count on us to guide you to the perfect property in your preferred neighborhood."
+    },
+    {
+        name: 'User-friendly interface',
+        description: "Searching for a home has never been easier. Our intuitive website lets you filter properties based on your preferences, making your house-hunting journey convenient and efficient."
+    },
+    {
+        name: 'Transparent Process',
+        description: "We believe in honesty and clarity. Our straightforward terms and accurate property information empower you to make informed decisions with confidence."
+    },
+    {
+        name: 'Verified Listings',
+        description: "Trust matters. Our listings are meticulously verified for accuracy, ensuring that what you see is exactly what you get."
+    },
+    {
+        name: 'Wide range of options',
+        description: "Whether you're looking for an apartment, house, or something in between, our diverse range of properties accommodates different budgets and lifestyles."
+    },
+    {
+        name: 'Tenenant Resources',
+        description: "Beyond finding your dream home, we're here to support you every step of the way. Access our guides on renting, moving tips, and tenant rights to navigate the rental process seamlessly."
+    }
+]
