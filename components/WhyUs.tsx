a903d535d6663a8088db887af39d69755aaad86b
Got-crypto <pecpalmplay@gmail.com>
2023-08-24 00:31:05 +0200

    designed whyus section

--- components/WhyUs.tsx
+++ components/WhyUs.tsx
@@ -5,11 +5,11 @@ import { whyUs } from "@/assets/constants"
 
 const WhyUs = () => {
   return (
-    <div className="w-full">
+    <div className="mt-5 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 md:gap-5">
       {whyUs.map((content, index) => (
-        <div key={index}>
-          <h1>{content.name}</h1>
-          <p>{content.description}</p>
+        <div key={index} className="w-full flex flex-col justify-start items-center">
+          <h1 className="text-2xl font-bold capitalize text-primary">{content.name}</h1>
+          <p className="text-gray-500 text-lg">{content.description}</p>
         </div>
       ))}
     </div>
