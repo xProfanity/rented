8b2deae11dea55434ce80d4775e093ea2c1855b8
Got-crypto <pecpalmplay@gmail.com>
2023-10-09 18:43:33 +0200

    successfully added stripe checkout

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -5,7 +5,14 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 export default async function handler(req, res) {
     if(req.method === 'POST') {
         try {
-            const price = req.body.price
+            const property = req.body
+            const propertyImages = property.images.map((image) => {
+                const imageRef = image.asset._ref
+                const imageUrl = imageRef.replace('image-', `https://cdn.sanity.io/images/${process.env.SANITY_PROJECT_ID}/production/`).replace('-webp', '.webp').replace('-jpeg', '.jpeg').replace('-png', '.png').replace('-jpg', '.jpg')
+
+                return imageUrl
+            })
+            const thumbnail = property.thumbnail.asset._ref.replace('image-', `https://cdn.sanity.io/images/${process.env.SANITY_PROJECT_ID}/production/`).replace('-webp', '.webp').replace('-jpeg', '.jpeg').replace('-png', '.png').replace('-jpg', '.jpg')
             const params = {
                 submit_type: 'pay',
                 mode: 'payment',
@@ -13,7 +20,17 @@ export default async function handler(req, res) {
                 billing_address_collection: 'auto',
                 line_items: [
                     {
-                        price,
+                        price_data: {
+                            currency: 'mwk',
+                            product_data: {
+                                name: property.title,
+                                images: [thumbnail, ...propertyImages]
+                            },
+                            unit_amount: property.price * 100
+                        },
+                        adjustable_quantity: {
+                            enabled: false
+                        },
                         quantity: 1,
                     },
                 ],
@@ -23,7 +40,7 @@ export default async function handler(req, res) {
             }
 
             const session = await stripe.checkout.sessions.create(params);
-            res.redirect(303, session.url);
+            res.status(200).json(session)
         
         } catch (error) {
             console.log('error.message', error.message)
