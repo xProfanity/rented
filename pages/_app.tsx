7eb328762e7c8ae6cfda20290178a65b6cc9aeeb
Got-crypto <pecpalmplay@gmail.com>
2023-10-09 23:58:03 +0200

    changed nprogress link stylesheet location

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { Footer, MobileMenu, Navbar } from '@/components'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import Router from 'next/router'
 import nProgress from 'nprogress'
 import 'react-loading-skeleton/dist/skeleton.css'
@@ -22,9 +21,6 @@ export default function App({ Component, pageProps: {session, ...pageProps} }: A
   })
   return (
     <>
-      <Head>
-        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
-      </Head>
       <SessionProvider session={session}>
         <StateContext>
           <Navbar />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head />
       <title>Rented</title>
       <link rel='icon' href='/logo.png' type='image/png'/>
+      <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
       <body>
         <Main />
         <NextScript />
