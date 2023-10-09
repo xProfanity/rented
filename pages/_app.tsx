e70822b8d4c57b1d33da70dbc55fbf2cb2e6143c
Got-crypto <pecpalmplay@gmail.com>
2023-10-09 23:29:24 +0200

    added nprogress component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,38 @@
 import { Footer, MobileMenu, Navbar } from '@/components'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Router from 'next/router'
+import nProgress from 'nprogress'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 import { StateContext } from '@/context/StateContext'
 import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+  nProgress.configure({
+    showSpinner: true,
+  })
+
+  Router.events.on('routeChangeStart', () => {
+    nProgress.start()
+  })
+  Router.events.on('routeChangeComplete', () => {
+    nProgress.done()
+  })
   return (
-    <SessionProvider session={session}>
-      <StateContext>
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-        <MobileMenu />
-      </StateContext>
-    </SessionProvider>
+    <>
+      <Head>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
+      </Head>
+      <SessionProvider session={session}>
+        <StateContext>
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+          <MobileMenu />
+        </StateContext>
+      </SessionProvider>
+    </>
   )
 }
