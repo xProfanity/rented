8980125256f8f0cf4066d86817190e5d5df925c7
Got-crypto <pecpalmplay@gmail.com>
2023-10-25 22:27:43 +0200

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,12 @@
-import { Property, User } from "@/common.types";
-import { HeroSection, Properties, Testimonials, TopFeatured, WhyUs } from "@/components";
-import { useStateContext } from '@/context/StateContext';
-import { fetchAllUsers, fetchFeaturedHouses, grabHouses } from "@/services/sanity";
+import { HeroSection, Testimonials, WhyUs } from "@/components";
 
-interface Props {
-  featured: Property[];
-  users: User[];
-  properties: Property[];
-}
 
-export default function Home({featured, users, properties}: Props) {
-  const {user}: any = useStateContext()
-  const loggedInUser = users?.filter((dbUser) => dbUser.email === user?.email)[0]
+
+export default function Home() {
 
   return (
     <div className="flex-1 h-auto flex flex-col ">
         <HeroSection />
-
-        <section className="mt-20 h-auto min-h-screen-w-full">
-          <Properties properties={properties} user={loggedInUser} />
-        </section>
-        
-        <section className="mt-20 h-auto min-h-screen w-full">
-          <TopFeatured featured={featured} user={loggedInUser} />
-        </section>
 
         <section className="mt-20 h-auto min-h-screen w-full">
           <WhyUs />
@@ -37,27 +20,3 @@ export default function Home({featured, users, properties}: Props) {
   )
 }
 
-
-export async function getServerSideProps() {
-  try {
-    const featured = await fetchFeaturedHouses()
-    const users = await fetchAllUsers()
-    const properties = await grabHouses()
-
-    return {
-        props: {
-            featured,
-            users,
-            properties
-        }
-    }
-  } catch (error) {
-    console.log('error', error)
-    return {
-      props: {
-        data: ''
-      }
-    }
-  }
-
-}
