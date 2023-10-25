2a4eeca9db9a5c184cbcaca9a631079f451c4a06
Got-crypto <pecpalmplay@gmail.com>
2023-10-25 22:22:06 +0200

    removed properties from home

---
+++ pages/properties.tsx
@@ -0,0 +1,54 @@
+import { Property, User } from "@/common.types";
+import { Properties, TopFeatured } from "@/components";
+import { useStateContext } from "@/context/StateContext";
+import { fetchAllUsers, fetchFeaturedHouses, grabHouses } from "@/services/sanity";
+
+interface Props {
+    featured: Property[];
+    users: User[];
+    properties: Property[];
+}
+
+export default function PropertiesPage({
+    featured,
+    users,
+    properties
+}: Props) {
+    const {user}: any = useStateContext()
+    const loggedInUser = users?.filter((dbUser) => dbUser.email === user?.email)[0]
+    return (
+        <>
+            <section className="mt-20 h-auto min-h-screen-w-full">
+            <Properties properties={properties} user={loggedInUser} />
+            </section>
+            
+            <section className="mt-20 h-auto min-h-screen w-full">
+            <TopFeatured featured={featured} user={loggedInUser} />
+            </section>
+        </>
+    )
+}
+
+export async function getServerSideProps() {
+    try {
+        const featured = await fetchFeaturedHouses()
+        const users = await fetchAllUsers()
+        const properties = await grabHouses()
+
+        return {
+            props: {
+                featured,
+                users,
+                properties
+            }
+        }
+    } catch (error) {
+        console.log('error', error)
+        return {
+            props: {
+                data: ''
+            }
+        }
+    }
+
+}
