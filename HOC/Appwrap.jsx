bb798acd7b1cba65b802bf6eed411ee1b77ca721
Got-crypto <pecpalmplay@gmail.com>
2023-11-04 23:25:51 +0200

    ...

--- HOC/Appwrap.jsx
+++ HOC/Appwrap.jsx
@@ -1,12 +1,16 @@
 const Appwrap = (Component, header, subtitle) => function HOC() {
   return (
     <div className="w-[95%] mt-36 mx-auto flex flex-col justify-start items-center">
-        <div className="w-full flex flex-row justify-center items-center">
-            <p className="text-6xl text-primary font-semibold capitalize">{header}</p>
-        </div>
+        {header && (
+          <div className="w-full flex flex-row justify-center items-center">
+              <p className="text-6xl text-primary font-semibold capitalize">{header}</p>
+          </div>
+        )}
 
         <div className={`mt-10 w-full`}>
-          <div className="text-lg text-gray-500 text-center w-full">{subtitle}</div>
+          {subtitle && (
+            <div className="text-lg text-gray-500 text-center w-full">{subtitle}</div>
+          )}
           <Component />
         </div>
     </div>
--- components/Overview.tsx
+++ components/Overview.tsx
@@ -1,11 +1,39 @@
-import Appwrap from "@/HOC/Appwrap"
+import Image, { StaticImageData } from 'next/image';
+
+import Appwrap from "@/HOC/Appwrap";
+import { overviews } from "@/assets";
 
 function Overview() {
   return (
-    <div className="">
-        Overview
+    <div className="min-h-screen h-auto py-16 w-full flex flex-col justify-center items-center">
+        <div className="h-full w-full flex flex-wrap justify-center items-center gap-10 flex-row">
+            {overviews.map((item, index) => (
+                <OverviewCard overview={item} key={index} />
+            ))}
+        </div>
     </div>
   )
 }
 
-export default Appwrap(Overview, "Welcome to rented", "overview")
+interface Overviews {
+    title: string;
+    subText: string;
+    image: StaticImageData;
+}
+
+interface Props {
+    overview: Overviews;
+}
+
+const OverviewCard = ({overview}: Props) => (
+    <div className='h-96 w-96 relative'>
+        <Image
+            src={overview.image}
+            alt='overview thumb'
+            fill
+            className='absolute object-cover rounded-sm md:rounded-lg xl:rounded-xl'
+        />
+    </div>
+)
+
+export default Appwrap(Overview)
