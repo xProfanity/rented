d0e23482f56a0899805d1b85c486b6c55a043f2c
Got-crypto <pecpalmplay@gmail.com>
2023-11-09 23:39:32 +0200

    ...

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <footer className="min-h-[10vh] h-auto bg-gray-200 w-full flex flex-col justify-center items-center">
+    <footer className="min-h-[10vh] h-auto mt-10 bg-gray-200 w-full flex flex-col justify-center items-center">
       <p className="text-sm text-gray-500">&copy; Gerrardafk 2023. All rights reserved.</p>
     </footer>
   )
